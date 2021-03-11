bd362ec67a152c0e162fe76b1e6113e2ea047955
Devin <devinjokeefe@gmail.com>
2021-03-11 17:15:23 +0100

    ConnectToDatabase file PHP

---
+++ ConnectToDb.php
@@ -0,0 +1,9 @@
+<?php
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'sweng';
+                    
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+?>
+
