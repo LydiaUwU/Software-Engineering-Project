a979a9271491b14c00ea32c217c99fa976adf105
itswilliamleonard <lolionai@tcd.ie>
2021-04-22 10:11:25 +0100

    Claim form now generates properly based on inputted claim

--- claimform.php
+++ claimform.php
@@ -1,5 +1,57 @@
 <?php
 
+session_start();                    // Start session variables
+include("connect-to-db.php");       // Connect to database
+include("helper-functions.php");    // Include helper functions
+
+if (!isLoggedIn()) {                // Page only accessible to logged in users
+    changePage("login.php");
+}
+
+if (!isAdmin($con)) {               // Page only accessible to admins
+    changePage("index.php");
+}
+
+include("header.php");
+
+// CLAIM DATA:
+
+$claimId = $_GET["claim"]; // Get claim ID
+
+// Select data from the submitted claim
+$sqlQuery = "SELECT dem_id, module_id, start_time, duration FROM claims WHERE claim_id = ?";
+$stmt = $con->prepare($sqlQuery);
+$stmt->bind_param("s", $claimId);
+$stmt->execute();
+
+$claimData = $stmt->get_result();
+$claimData = $claimData->fetch_all();
+
+// Separate claim data
+foreach ($claimData as $claim) {
+    // Assign variables based on query response
+    $instructorID = $claim[0];
+    $moduleName = $claim[1];
+    $startDatetime = new DateTime($claim[2]);
+    $startDay = $startDatetime->format("D");
+    $duration = $claim[3];
+
+    // Get instructor data
+    $sqlQuery = "SELECT name FROM accounts WHERE id = ?";
+    $stmt = $con->prepare($sqlQuery);
+    $stmt->bind_param("s", $instructorID);
+    $stmt->execute();
+
+    $demName= $stmt->get_result();
+    $demName= $demName->fetch_all();
+}
+
+// Get instructor data
+
+
+
+// CLAIM FORM:
+
 // import TCPDF
 require_once('libraries/tcpdf_min/tcpdf.php'); // point this to tcpdf location - get it from https://github.com/tecnickcom/tcpdf
 
@@ -17,7 +69,7 @@ $pdf->SetAutoPageBreak(TRUE, 5);
 $pdf->AddPage();
 $pdf->SetFont('helvetica','', 9);
 
-$html = <<<EOF
+$html = '
     <style>
         h1, h2, h3, p {
             text-align:center;
@@ -56,9 +108,9 @@ $html = <<<EOF
         <!-- ASSUME MONTHLY PAYROLL -->
         <tr>
             <td class="formLabel">Surname:</td>
-            <td class="formInput"><!--SURNAME--></td>
+            <td class="formInput">' . $demName[0][0] . '</td>
             <td class="formLabel">First Names:</td>
-            <td class="formInput"><!--FIRST NAMES--></td>
+            <td class="formInput">' . $demName[0][0] . '</td>
             <td class="formLabel">PPS No:</td>
             <td class="formInput"><!--PPS NO--></td>
             <td class="formLabel">Staff No:</td>
@@ -127,7 +179,7 @@ $html = <<<EOF
             <td class="formLabel">Course Title:</td>
             <td class="formInput" colspan="3"><!--COURSE TITLE--></td>
             <td class="formLabel" colspan="3">Course Module:</td>
-            <td class="formInput" colspan="3"><!--COURSE MODULE--></td>
+            <td class="formInput" colspan="3">' . $moduleName . '</td>
             <td class="formLabel">Signature of Course Director/Lecturer:</td>
             <td class="formInput" colspan="2"><!--COURSE DIRECTOR/LECTURER--></td>
             <td class="formOfficial" colspan="2"></td>
@@ -157,16 +209,16 @@ $html = <<<EOF
         </tr>
         <tr>
             <td class="formLabel">Week Ended:</td>
-            <td class="formInput"><!--WEEK ENDED 1--></td>
-            <td class="formInput"><!--SATURDAY 1--></td>
-            <td class="formInput"><!--SUNDAY 1--></td>
-            <td class="formInput"><!--MONDAY 1--></td>
-            <td class="formInput"><!--TUESDAY 1--></td>
-            <td class="formInput"><!--WEDNESDAY 1--></td>
-            <td class="formInput"><!--THURSDAY 1--></td>
-            <td class="formInput"><!--FRIDAY 1--></td>
+            <td class="formInput">' . $startDatetime->format('d-m Y') . '</td>
+            <td class="formInput">' . ($startDay == "Sat" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Sun" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Mon" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Tue" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Wed" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Thu" ? $duration : '0.00') . '</td>
+            <td class="formInput">' . ($startDay == "Fri" ? $duration : '0.00') . '</td>
             <td class="formInput"><!--DESCRIPTION OF WORK 1--></td>
-            <td><!--TOTAL HOURS 1--></td>
+            <td>' . $duration . '</td>
             <td><!--HOURLY RATE 1--></td>
             <td><!--GROSS PAY 1--></td>
             <td class="formOfficial"><!--PRSI CLASS 1--></td>
@@ -257,16 +309,16 @@ $html = <<<EOF
     <table>
         <tr>
             <td class="formLabel">Signature of person seeking payment:</td>
-            <td class="formInput"><!--DEMONSTRATOR NAME--></td>
+            <td class="formInput">' . $demName[0][0] . '</td>
             <td class="formLabel">Date:</td>
-            <td class="formInput"><!--DATE--></td>
+            <td class="formInput">' . date("d-m-Y") . '</td>
             <td class="formLabel">Signature of Head of School/TRI or Project A/C Holder:</td>
             <td class="formInput"><!--ADMIN NAME--></td>
             <td class="formLabel">Date:</td>
             <td class="formInput"><!--DATE--></td>
         </tr>
     </table>
-    EOF;
+    ';
 
 // output the HTML content
 $pdf->writeHTML($html, true, false, true, false, '');
@@ -275,6 +327,7 @@ $pdf->writeHTML($html, true, false, true, false, '');
 $pdf->lastPage();
 
 // Close and output PDF document
+ob_end_clean();
 $pdf->Output('claim-form.pdf', 'I'); // this opens the finished PDF in the browser - user can download if they wish.
 
 ?>
