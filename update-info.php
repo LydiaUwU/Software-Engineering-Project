2d9b73b592ecbae70810f7df1650e66233cd88ad
itswilliamleonard <lolionai@tcd.ie>
2021-04-22 18:21:27 +0100

    Added login support to update-info.php

--- update-info.php
+++ update-info.php
@@ -4,6 +4,7 @@
     session_start();                    // Start session cookies
     include("connect-to-db.php");       // Connect to database
     include("helper-functions.php");    // Access helper functions
+    include("header.php");
 
     // Declare constants
     define("INVALID_EMAIL", "Unfortunately this email has an invalid format. Please try again.");
@@ -106,37 +107,6 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Hello World!</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Font Imports! |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <!-- Inter -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <!-- Karrik -->
-    <!-- TODO: Create basic logo concept. -->
-    <link href="fonts/karrik/karrik.css" rel="stylesheet">
-</head>
-<body>
-<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Navigation Bar |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-<div id="navbar">
-    <h1>Logo-Here</h1>
-    <div id="nav-links">
-        <ul class="nav-ul">
-            <li class="nav-li"><a href="index.php" class="nav-link">Home</a> </li>
-            <li class="nav-li"><a href="instructor.php" class="nav-link-act">Instructor</a></li> <!-- Current Page! -->
-            <li class="nav-li"><a href="admin.php" class="nav-link">Admin</a></li>
-            <li class="nav-li"><a href="index.php" class="nav-link">Help</a></li>
-        </ul>
-    </div>
-    <div id="nav-user">
-        <!-- TODO: Login page -->
-        <a href="login.php" class="nav-link">John Doe</a>
-    </div>
-</div>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Page Contents |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 <!-- TODO: Create basic table and button elements -->
 <div id="foreground">
